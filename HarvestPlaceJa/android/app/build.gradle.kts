3271588f4c7659855bacde70f170f398108c8495
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-06-02 00:24:32 -0500

    Update versionCode and versionName in build.gradle.kts

--- HarvestPlaceJa/android/app/build.gradle.kts
+++ HarvestPlaceJa/android/app/build.gradle.kts
@@ -22,8 +22,10 @@ android {
         applicationId = "com.harvestplaceja.myapp"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+
+        // Google Play requires this to increase for every new upload.
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     val cmKeystorePath = System.getenv("CM_KEYSTORE_PATH")

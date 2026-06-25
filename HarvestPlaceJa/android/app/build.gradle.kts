9cdd7bc9181f0dd944d77eb6e49a69059b9f9718
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-06-24 21:43:00 -0500

    Increment versionCode from 14 to 15

--- HarvestPlaceJa/android/app/build.gradle.kts
+++ HarvestPlaceJa/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         targetSdk = flutter.targetSdkVersion
 
         // Google Play requires this to increase for every new upload.
-        versionCode = 14
+        versionCode = 15
         versionName = "1.0.3"
     }
 

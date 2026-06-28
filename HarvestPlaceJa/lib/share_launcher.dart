43f95f94d5bff51a8844f1db8934e91c7257c2a8
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-06-28 15:40:21 -0500

    Add mobile export for share_launcher

--- HarvestPlaceJa/lib/share_launcher.dart
+++ HarvestPlaceJa/lib/share_launcher.dart
@@ -1,2 +1,3 @@
 export 'share_launcher_stub.dart'
-    if (dart.library.html) 'share_launcher_web.dart';
+    if (dart.library.html) 'share_launcher_web.dart'
+    if (dart.library.io) 'share_launcher_mobile.dart';

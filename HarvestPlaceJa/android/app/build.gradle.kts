6f669ebe4d0346c2f179c1018cfa9534814026e1
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-06-28 01:05:15 -0500

    Update versionCode to 20
    Increment versionCode to meet Google Play requirements.

--- HarvestPlaceJa/android/app/build.gradle.kts
+++ HarvestPlaceJa/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         targetSdk = flutter.targetSdkVersion
 
         // Google Play requires this to increase for every new upload.
-        versionCode = 19
+        versionCode = 20
         versionName = "1.0.3"
     }
 

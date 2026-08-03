5afe65a620f022d509e88b6d7c1dd937f7f4d320
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-08-02 22:25:31 -0500

    Update build.gradle.kts

--- HarvestPlaceJa/android/app/build.gradle.kts
+++ HarvestPlaceJa/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         targetSdk = flutter.targetSdkVersion
 
         // Google Play requires this to increase for every new upload.
-        versionCode = 29
+        versionCode = 30
         versionName = "1.0.3"
     }
 

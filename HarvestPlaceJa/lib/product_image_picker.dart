38ef03a2043a4462c5186a5430a7f23d06906a10
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-08-30 01:06:08 -0500

    Add files via upload

--- HarvestPlaceJa/lib/product_image_picker.dart
+++ HarvestPlaceJa/lib/product_image_picker.dart
@@ -1,3 +1,2 @@
 export 'product_image_picker_stub.dart'
-    if (dart.library.html) 'product_image_picker_web.dart'
-    if (dart.library.io) 'product_image_picker_io.dart';
+    if (dart.library.html) 'product_image_picker_web.dart';

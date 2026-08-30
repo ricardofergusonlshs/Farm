d344203ae85ab519bdf58cc2209db3ce219be69e
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-08-30 13:19:39 -0500

    Increment versionCode to 37 for new release

--- HarvestPlaceJa/android/app/build.gradle.kts
+++ HarvestPlaceJa/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         targetSdk = flutter.targetSdkVersion
 
         // Increase for every new Google Play upload.
-        versionCode = 36
+        versionCode = 37
         versionName = "1.0.4"
     }
 

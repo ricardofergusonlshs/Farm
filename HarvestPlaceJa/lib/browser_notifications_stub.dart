1b682f92ea752cacb47fbc218747e10ff017e0e3
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-09-01 00:27:29 -0500

    Clarify non-web notification implementation
    Add comment clarifying non-web implementation for notifications.

--- HarvestPlaceJa/lib/browser_notifications_stub.dart
+++ HarvestPlaceJa/lib/browser_notifications_stub.dart
@@ -1,5 +1,7 @@
 // Non-web implementation.
+//
 // Android/iOS/desktop use native push notifications instead.
+// Browser notification APIs do not exist on these platforms.
 
 Future<bool> requestBrowserNotifications() async {
   return false;

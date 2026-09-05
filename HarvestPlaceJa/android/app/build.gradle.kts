b8461e13f56a0ca6583dac2f9a55180fa7af9644
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-09-05 11:00:12 -0500

    Bump versionCode to 40 for Play Store upload

--- HarvestPlaceJa/android/app/build.gradle.kts
+++ HarvestPlaceJa/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         targetSdk = flutter.targetSdkVersion
 
         // Increase for every new Google Play upload.
-        versionCode = 39
+        versionCode = 40
         versionName = "1.0.4"
     }
 

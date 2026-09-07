9b1b9f0f14123865fcf86991ff0055133bd2c715
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-09-06 20:59:00 -0500

    Bump versionCode from 44 to 45

--- HarvestPlaceJa/android/app/build.gradle.kts
+++ HarvestPlaceJa/android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         targetSdk = flutter.targetSdkVersion
 
         // Must increase for each Google Play upload.
-        versionCode = 44
+        versionCode = 45
         versionName = "1.0.4"
     }
 
